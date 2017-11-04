8ef837e2dde317bec83f9b0f1954e42be12dfaac
chriswongatcuhk <wc38.wong@gmail.com>
2017-11-04 20:08:52 +0800

    Creating separate components to display a list of todo’s and a single todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import {TodoDataService} from './todo-data.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 export class AppComponent {
-  newTodo: Todo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // newTodo: Todo = new Todo();
 
   // Ask Angular DI system to inject the dependency
   // associated with the dependency injection token `TodoDataService`
@@ -18,17 +19,24 @@ export class AppComponent {
   constructor(private todoDataService: TodoDataService) {
   }
 
-  // Service is now available as this.todoDataService
-  addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //    this.newTodo = new Todo();
+  // }
+
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
 
-  toggleTodoComplete(todo) {
+  // rename from toggleTodoComplete
+  onToggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
+  // rename from removeTodo
+  onRemoveTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
 
